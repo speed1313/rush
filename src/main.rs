e29b45e7c0e43dd0c2dcc65b8c3a00229d2121e7
speed1313 <speedtry13@icloud.com>
2022-01-07 23:47:25 +0900

    impl loopable shell

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+use std::io::{stdin, stdout, Write};
+use std::path::Path;
+use std::process::{Child, Command, Stdio};
 fn main() {
-    println!("Hello, world!");
+    loop {
+        // promptを出力
+        print!("> ");
+        stdout().flush();
+
+        let mut input = String::new();
+        // 改行されるまで受け取りinputに格納.
+        stdin().read_line(&mut input).unwrap();
+
+        // 改行文字を除く
+        let command = input.trim();
+
+        let mut child = Command::new(command).spawn().unwrap();
+
+        // wait a child process if this process bear it.
+        child.wait();
+    }
 }
